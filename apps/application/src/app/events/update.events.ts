7111d7078c42fb1d79b84728337fe40f5b61d487
ruohki <ruohki@gmail.com>
2020-06-30 00:07:24 +0200

    Auto update fix

--- apps/application/src/app/events/update.events.ts
+++ apps/application/src/app/events/update.events.ts
@@ -1,4 +1,5 @@
-import { app, autoUpdater, dialog } from 'electron';
+import { app, dialog } from 'electron';
+import { autoUpdater } from 'electron-updater';
 import { platform, arch } from 'os';
 import { updateServerUrl } from '../constants';
 import App from '../app';
